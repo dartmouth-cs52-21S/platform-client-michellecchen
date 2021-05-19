9a72c5f4e77e594ee71996bef9d83543a1591db9
Michelle Chen <michelle.chen.22@dartmouth.edu>
2021-05-18 17:39:33 -0700

    hm

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const ROOT_URL = 'http://localhost:9090/api';
+//const ROOT_URL = 'http://localhost:9090/api';
+const ROOT_URL = 'https://blog-michellecchen.herokuapp.com/api';
 const API_KEY = '?key=michelle_chen';
 
 // keys for actiontypes
